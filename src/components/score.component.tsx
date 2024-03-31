ba09f2673aaf2eaae7fe9207edd64a2685d57728
Wunna <mgwunna.mw@icloud.com>
2024-03-31 19:15:06 +0630

    fix: 🐛 missing logo in score component error

--- src/components/score.component.tsx
+++ src/components/score.component.tsx
@@ -1,16 +1,14 @@
+import logo from "../../images/logo.svg";
+
 type ScoreComponentProps = {
   score: number;
 };
 
 const ScoreComponent = ({ score }: ScoreComponentProps) => {
   return (
-    <div className="border-neutral-header-outline mx-auto flex w-full items-center justify-between rounded-md border-2 border-solid p-3">
-      <div className="text-2xl font-bold uppercase leading-5 text-white">
-        <p>Rock</p>
-        <p>Paper</p>
-        <p>Scissors</p>
-      </div>
-      <div className="aspect-square rounded-md bg-white p-2 px-4 text-center">
+    <div className="border-neutral-header-outline mx-auto flex w-full items-center justify-between rounded-md border-2 border-solid p-4 px-6">
+      <img className="w-24" src={logo} />
+      <div className="flex aspect-square h-full flex-col items-center justify-center rounded-md bg-white p-2 px-4 text-center">
         <p className="text-neutral-score-text uppercase">Score</p>
         <p className="text-neutral-dark-text text-3xl font-bold">{score}</p>
       </div>
